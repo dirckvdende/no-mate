5ad2b74033e485386dbdfd749d63d9d954345309
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-05-15 14:30:19 +0200

    Integrated useViewTransform into Puzzle component

--- src/components/Puzzle.vue
+++ src/components/Puzzle.vue
@@ -1,60 +1,41 @@
 <script setup lang="ts">
+    import { useViewTransform } from "@/composables/useViewTransform"
     import type { Puzzle } from "@/types/Puzzle"
-    import { puzzleRender } from "@/util/puzzleRender";
-    import { useElementSize } from "@vueuse/core";
-    import { computed, useTemplateRef } from "vue";
+    import { puzzleRender } from "@/util/puzzleRender"
+    import { computed, useTemplateRef } from "vue"
 
     const { puzzle } = defineProps<{
         /** The puzzle to show */
         puzzle: Puzzle
     }>()
 
+    const container = useTemplateRef("puzzle")
     const render = computed(() => puzzleRender(puzzle))
-    const {
-        width: containerWidth,
-        height: containerHeight,
-    } = useElementSize(useTemplateRef("puzzle"))
+    const renderSize = computed<[number, number]>(() => [
+        render.value.bottomRight[0] - render.value.topLeft[0],
+        render.value.bottomRight[1] - render.value.topLeft[1],
+    ])
 
-    const renderSize = computed<[number, number]>(() => {
-        const { topLeft, bottomRight } = render.value
-        return [
-            bottomRight[0] - topLeft[0],
-            bottomRight[1] - topLeft[1],
-        ]
-    })
-
-    /**
-     * Convert a position given by the puzzle render to a pixel position
-     * @param position Position as given by the puzzle render
-     * @returns The pixel position for display inside the puzzle container
-     */
-    function displayPosition(position: [number, number]): [number, number] {
-        const { topLeft } = render.value
-        const [width, height] = renderSize.value
-        return [
-            (position[0] - topLeft[0]) / width * containerWidth.value,
-            (position[1] - topLeft[1]) / height * containerHeight.value,
-        ]
-    }
-
-    const puzzleAspectRatioStyle = computed(() => {
+    const containerStyle = computed(() => { 
         const [width, height] = renderSize.value
         if (height / width > 1.4)
             return `height: 35em; aspect-ratio: ${width} / ${height}`
         return `width: 25em; aspect-ratio: ${width} / ${height}`
     })
 
-    const pixelRatio = computed(() =>
-        containerWidth.value / renderSize.value[0])
+    const { pixelSize, toPixelCoords } = useViewTransform(
+        container,
+        computed(() => [render.value.topLeft, render.value.bottomRight]),
+    )
 
     const spaceStyles = computed(() => {
         return render.value.spaces.map((space) => {
-            const position = displayPosition(space.position)
+            const position = toPixelCoords(space.position)
             return `
                 top: ${position[1]}px;
                 left: ${position[0]}px;
-                width: ${pixelRatio.value}px;
-                height: ${pixelRatio.value}px;
+                width: ${1 / pixelSize.value}px;
+                height: ${1 / pixelSize.value}px;
             `
         })
     })
@@ -65,7 +46,7 @@
         <div
             :class="$style.puzzle"
             ref="puzzle"
-            :style="puzzleAspectRatioStyle">
+            :style="containerStyle">
             <div
                 v-for="spaceStyle in spaceStyles"
                 :style="spaceStyle"
