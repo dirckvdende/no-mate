480d575ef2a97b1c0eca4c6e4cbcdc35c2578a77
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-06-02 15:08:53 +0200

    Updated puzzle 2

--- src/puzzles/2.ts
+++ src/puzzles/2.ts
@@ -60,9 +60,7 @@ const piece2: Piece = {
 export const puzzle: Puzzle = {
     spaces: [
         { x: 0, y: 0 },
-        { x: 2, y: 0 },
         { x: 0, y: 1 },
-        { x: 2, y: 1 },
         { x: 0, y: 2 },
         { x: 1, y: 2 },
         { x: 2, y: 2 },
