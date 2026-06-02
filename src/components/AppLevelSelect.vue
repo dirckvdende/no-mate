1df49e0684e8f05bd0f7c6007b28f5bb767ada33
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-06-02 13:44:09 +0200

    Updated level selection style

--- src/components/AppLevelSelect.vue
+++ src/components/AppLevelSelect.vue
@@ -17,44 +17,87 @@
 <template>
     <h1>Tomaat</h1>
     <div :class="$style['level-list']">
-        <button
-            v-for="_, id in puzzles"
-            :class="$style.level"
-            @click="loadLevel(id)">
-            #{{ id }}
-            <div
-                v-if="completedPuzzles.find(v => v == id)"
-                :class="$style.checkmark">
-                <Icon :path="mdiCheckBold" :class="$style.icon" />
-            </div>
-        </button>
+        <div :class="$style.list">
+            <template v-for="_ in 100">
+                <button
+                    v-for="_, id in puzzles"
+                    :class="$style.level"
+                    @click="loadLevel(id)">
+                    #{{ id }}
+                    <div
+                        v-if="completedPuzzles.find(v => v == id)"
+                        :class="$style.checkmark">
+                        <Icon :path="mdiCheckBold" :class="$style.icon" />
+                    </div>
+                </button>
+            </template>
+        </div>
+        <div :class="$style['fade-top']" />
+        <div :class="$style['fade-bottom']" />
     </div>
+    <p :class="$style.credit">
+        Created by Dirck van den Ende<br />
+        <a href="https://dirck.dev/projects">View more projects</a>
+    </p>
 </template>
 
 <style lang="scss" module>
     h1 {
-        font-size: 3em;
+        font-size: 3.5em;
         margin: 1.5em 0 .5em 0;
     }
 
     .level-list {
         flex-shrink: 1;
-        width: 100%;
-        overflow-x: hidden;
-        overflow-y: auto;
+        flex-grow: 1;
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         justify-content: center;
-        flex-wrap: wrap;
-        scrollbar-color: #222 transparent;
-        margin-bottom: 4em;
-        padding: 1em 0;
+        align-items: center;
+        width: 100%;
+        height: 0;
+        margin-bottom: 1em;
+        position: relative;
+        
+        .list {
+            width: 100%;
+            flex-shrink: 1;
+            flex-grow: 1;
+            overflow-x: hidden;
+            overflow-y: auto;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            flex-wrap: wrap;
+            scrollbar-color: #222 transparent;
+            padding: 1em 0;
+        }
+
+        .fade-top {
+            position: absolute;
+            height: 1.5em;
+            top: -.1em;
+            left: 0;
+            width: 100%;
+            background: linear-gradient(#eee, transparent);
+            pointer-events: none;
+        }
+
+        .fade-bottom {
+            position: absolute;
+            height: 1.5em;
+            bottom: -.1em;
+            left: 0;
+            width: 100%;
+            background: linear-gradient(transparent, #eee);
+            pointer-events: none;
+        }
     }
 
     .level {
-        width: 3em;
-        height: 3em;
-        font-size: 2em;
+        width: 2.6em;
+        height: 2.6em;
+        font-size: 3em;
         background-color: #ddd;
         border-radius: .3em;
         box-sizing: border-box;
@@ -73,8 +116,8 @@
             display: flex;
             justify-content: center;
             align-items: center;
-            width: 1em;
-            height: 1em;
+            width: .9em;
+            height: .9em;
             right: -.2em;
             top: -.2em;
             background-color: #1c9c0e;
@@ -85,6 +128,18 @@
                 width: 65%;
                 height: 65%;
             }
+        }
+    }
+
+    .credit {
+        text-align: center;
+        color: #666;
+        font-size: .8em;
+        margin-bottom: 1em;
+
+        & > a {
+            color: inherit;
+            text-decoration: underline;
         }
     }
 </style>
