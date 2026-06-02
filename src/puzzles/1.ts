d52a07b65689ce52c578543e9c27796a8aa9672e
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-06-02 17:28:29 +0200

    Moved puzzle pieces to separate file

--- src/puzzles/1.ts
+++ src/puzzles/1.ts
@@ -1,34 +1,6 @@
 
-import type { Position } from "@/types/position"
-import type { Puzzle, Piece } from "@/types/puzzle"
-import { mdiChessBishop } from "@mdi/js"
-
-const piece: Piece = {
-    name: "piece",
-    icon: mdiChessBishop,
-    color: "#9500ff",
-    isValid: ({ isSpace, pieceAtPosition }, position) => {
-
-        function followRay(diff: Position): boolean {
-            let currentPos: Position = { ...position }
-            currentPos.x += diff.x
-            currentPos.y += diff.y
-            while (isSpace(currentPos)) {
-                if (pieceAtPosition(currentPos))
-                    return false
-                currentPos.x += diff.x
-                currentPos.y += diff.y
-            }
-            return true
-        }
-
-        return followRay({ x: 1, y: 1 })
-            && followRay({ x: 1, y: -1 })
-            && followRay({ x: -1, y: 1 })
-            && followRay({ x: -1, y: -1 })
-
-    },
-}
+import type { Puzzle } from "@/types/puzzle"
+import { bishop } from "./pieces"
 
 export const puzzle: Puzzle = {
     spaces: [
@@ -36,5 +8,5 @@ export const puzzle: Puzzle = {
         { x: 0, y: 1 },
         { x: 1, y: 0 },
     ],
-    pieces: [piece, piece],
+    pieces: [bishop, bishop],
 }
---
+++ src/puzzles/pieces.ts
@@ -0,0 +1,58 @@
+
+import type { Piece } from "@/types/puzzle"
+import type { Position } from "@/types/position"
+import { mdiChessBishop, mdiChessRook } from "@mdi/js"
+
+export const bishop: Piece = {
+    name: "bishop",
+    icon: mdiChessBishop,
+    color: "#9500ff",
+    isValid: ({ isSpace, pieceAtPosition }, position) => {
+
+        function followRay(diff: Position): boolean {
+            let currentPos: Position = { ...position }
+            currentPos.x += diff.x
+            currentPos.y += diff.y
+            while (isSpace(currentPos)) {
+                if (pieceAtPosition(currentPos))
+                    return false
+                currentPos.x += diff.x
+                currentPos.y += diff.y
+            }
+            return true
+        }
+
+        return followRay({ x: 1, y: 1 })
+            && followRay({ x: 1, y: -1 })
+            && followRay({ x: -1, y: 1 })
+            && followRay({ x: -1, y: -1 })
+
+    },
+}
+
+export const rook: Piece = {
+    name: "rook",
+    icon: mdiChessRook,
+    color: "#ff9900",
+    isValid: ({ isSpace, pieceAtPosition }, position) => {
+
+        function followRay(diff: Position): boolean {
+            let currentPos: Position = { ...position }
+            currentPos.x += diff.x
+            currentPos.y += diff.y
+            while (isSpace(currentPos)) {
+                if (pieceAtPosition(currentPos))
+                    return false
+                currentPos.x += diff.x
+                currentPos.y += diff.y
+            }
+            return true
+        }
+
+        return followRay({ x: 1, y: 0 })
+            && followRay({ x: -1, y: 0 })
+            && followRay({ x: 0, y: 1 })
+            && followRay({ x: 0, y: -1 })
+
+    },
+}
