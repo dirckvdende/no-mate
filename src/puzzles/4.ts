550781465df30644f03e8fba2b789c62aa51e581
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-06-03 22:31:57 +0200

    Added fourth puzzle

---
+++ src/puzzles/4.ts
@@ -0,0 +1,15 @@
+
+import type { Puzzle } from "@/types/puzzle"
+import { knight, pawn } from "./pieces"
+
+export const puzzle: Puzzle = {
+    spaces: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 0 },
+        { x: 2, y: 1 },
+    ],
+    pieces: [pawn, pawn, pawn, pawn, knight],
+}
--- src/puzzles/puzzles.ts
+++ src/puzzles/puzzles.ts
@@ -4,4 +4,5 @@ export const puzzles = {
     '1': (await import("./1")).puzzle,
     '2': (await import("./2")).puzzle,
     '3': (await import("./3")).puzzle,
+    '4': (await import("./4")).puzzle,
 } as const
