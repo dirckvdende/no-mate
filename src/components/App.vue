fcc5acd05ff5da74c10ac6e0361f4999686d11ef
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-06-04 19:25:35 +0200

    Fixed bug with large differences between max viewport and actual viewport

--- src/components/App.vue
+++ src/components/App.vue
@@ -21,10 +21,10 @@
     body {
         background-color: #eee;
         margin: 0;
-        --true-width: calc(100lvw
+        --true-width: calc(100vw
             - env(safe-area-inset-left, 0px)
             - env(safe-area-inset-right, 0px));
-        --true-height: calc(100lvh
+        --true-height: calc(100vh
             - env(safe-area-inset-top, 0px)
             - env(safe-area-inset-bottom, 0px));
         font-size: min(
