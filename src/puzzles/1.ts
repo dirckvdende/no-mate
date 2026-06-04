87aaff57ab91c6edf47020e01c8cd5d2ba3be34e
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-06-04 16:27:02 +0200

    Updated puzzle 1

--- src/puzzles/1.ts
+++ src/puzzles/1.ts
@@ -1,12 +1,13 @@
 
 import type { Puzzle } from "@/types/puzzle"
-import { bishop } from "./pieces"
+import { rook } from "./pieces"
 
 export const puzzle: Puzzle = {
     spaces: [
         { x: 0, y: 0 },
         { x: 0, y: 1 },
         { x: 1, y: 0 },
+        { x: 1, y: 1 },
     ],
-    pieces: [bishop, bishop],
+    pieces: [rook, rook],
 }
