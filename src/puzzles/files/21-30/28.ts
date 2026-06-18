de439970737a8dd11388eae2c1d9203c04ddc389
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-06-18 19:32:31 +0200

    Added five more puzzles

---
+++ src/puzzles/files/21-30/28.ts
@@ -0,0 +1,23 @@
+
+import type { Puzzle } from "@/types/puzzle"
+import { pawn, knight, bishop, king, queen } from "@/puzzles/pieces"
+
+export const puzzle: Puzzle = {
+    spaces: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 3, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 0, y: 2 },
+        { x: 1, y: 2 },
+        { x: 2, y: 2 },
+        { x: 3, y: 2 },
+        { x: 0, y: 3 },
+        { x: 1, y: 3 },
+        { x: 2, y: 3 },
+        { x: 3, y: 3 },
+    ],
+    pieces: [ king, bishop, knight, queen, pawn ],
+}
